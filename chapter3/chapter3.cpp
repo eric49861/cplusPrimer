07b44e9e05420cf3aeca28245b3699bbd7f2f762
eric <15854168262@qq.com>
2022-12-15 21:22:14 +0800

    add some new practices

--- chapter3/chapter3.cpp
+++ chapter3/chapter3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -17,12 +18,18 @@ void practice_37();
  * 读入一个包含标点符号的字符串，去除标点符号输出字符串剩余的部分
  */
 void practice_310();
-
-
+/**
+ * 练习3.14
+ * 用cin读入一组整数，并把他们存入vector中
+ */
+void practice_314();
+/**
+ * 练习3.15
+ * 改写3.14，读入字符串
+ */
+void practice_315();
 int main(int argc, char **argv){
-	practice_36();
-	practice_37();
-	practice_310();
+	practice_314();
 	return 0;
 }
 
@@ -71,5 +78,23 @@ void practice_310(){
 			}
 		}
 		cout << '\n';
+	}
+}
+
+void practice_314()
+{
+	vector<int> nums;
+	int num = 0;
+	while(cin >> num){
+		nums.push_back(num);
+	}
+}
+
+void practice_315()
+{
+	vector<string> strs;
+	string s;
+	while(cin >> s){
+		strs.push_back(s);
 	}
 }
